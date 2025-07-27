597eacd59620faa841d00d3365d288ca1f3f3a92
jqpython <168331391+jqpython@users.noreply.github.com>
2025-07-27 14:03:56 +0000

    max board size changed back to 5

--- 042_challenge_2_exercise.py
+++ 042_challenge_2_exercise.py
@@ -255,7 +255,7 @@ def get_board_size_from_user():
     Handles invalid inputs and ensures the size is within valid bounds.
     """
     MIN_BOARD_SIZE = 3
-    MAX_BOARD_SIZE = 10
+    MAX_BOARD_SIZE = 5
 
     while True:
         try:
